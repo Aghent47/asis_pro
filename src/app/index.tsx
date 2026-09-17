b276137a2abf40f3a9a49fbd1b0084d126457e9f
Neider Hernández M. <44579954+Aghent47@users.noreply.github.com>
2026-09-17 18:54:58 -0500

    Update text and styles in App component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,26 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Asistencia</Text>
+      <Text style={styles.text}>Neider Hernandez</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Us
+      </Link>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  button: {
+    fontSize: 18,
+    color: "#fff",
+    backgroundColor: "#415252",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#11c1c1",
+    backgroundColor: "#415252",
   },
   text: {
     color: "#fff",
